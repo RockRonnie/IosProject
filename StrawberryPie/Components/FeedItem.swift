0e5aba0b40100c74762da8eeac1c401bfe940918
iosdev <ron@johnson.dom>
2019-11-22 20:51:33 +0200

    classes

---
+++ StrawberryPie/Components/FeedItem.swift
@@ -0,0 +1,24 @@
+//
+//  FeedItem.swift
+//  StrawberryPie
+//
+//  Created by iosdev on 22/11/2019.
+//  Copyright © 2019 Team Työkkäri. All rights reserved.
+//
+
+import UIKit
+
+class FeedItem: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
