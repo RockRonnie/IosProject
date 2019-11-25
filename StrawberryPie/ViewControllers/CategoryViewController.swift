1530f64b3fae093d2692eb836e21541626f104ba
Arttu Jokinen <arttujo@gmail.com>
2019-11-25 13:15:04 +0200

    More CoreData changes

--- StrawberryPie/ViewControllers/CategoryViewController.swift
+++ StrawberryPie/ViewControllers/CategoryViewController.swift
@@ -11,52 +11,66 @@ import CoreData
 
 class CategoryViewController: UIViewController {
     
-    @IBOutlet weak var testOutlet: UILabel!
     
-    func createCategoryData(){
+    func createCategoryData(name: String, summary: String){
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
         let managedContext = appDelegate.persistentContainer.viewContext
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Category")
+        fetchRequest.predicate = NSPredicate(format: "categoryName == %@", name)
         do {
             let result = try managedContext.fetch(fetchRequest)
             if result.count < 1 {
                 let categoryEntity = NSEntityDescription.entity(forEntityName: "Category", in: managedContext)!
                 let category = NSManagedObject(entity: categoryEntity, insertInto: managedContext)
-                category.setValue("ICT", forKeyPath: "categoryName")
-                category.setValue("Computers and stuff", forKey: "categorySummary")
+                category.setValue(name, forKeyPath: "categoryName")
+                category.setValue(summary, forKey: "categorySummary")
                 try managedContext.save()
-                print("Succesfully saved data!!!")
+                print("Succesfully saved data for category \(name)")
             } else {
                 print("Object found. Didn't set")
             }
         }catch let error as NSError{
             print("Could not save. \(error), \(error.userInfo)")
         }
-        
     }
     
-    func getCategoryData(){
+    func getAllCategoryData(){
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
-        
         let managedContext = appDelegate.persistentContainer.viewContext
-        
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Category")
         
         do {
             let result = try managedContext.fetch(fetchRequest)
             print("Results: \(result)")
             for data in result as! [NSManagedObject]{
-                print(data.value(forKey: "categoryName")as! Any)
-                print(data.value(forKey: "categorySummary")as! Any)
+                print("Name of category: \(data.value(forKey: "categoryName")as! String)")
+                print("Summary: \(data.value(forKey: "categorySummary")as! String)")
             }
         } catch {
             print(error)
         }
     }
+    
+    func getCategoryData(name: String){
+        guard let appDel = UIApplication.shared.delegate as? AppDelegate else {return}
+        let managedContext = appDel.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Category")
+        fetchRequest.predicate = NSPredicate(format: "categoryName == %@", name)
+        
+        do {
+            let result = try managedContext.fetch(fetchRequest) as! [NSManagedObject]
+            print(result[0].value(forKey: "categoryName") as! String)
+            print(result[0].value(forKey: "categorySummary") as! String)
+            
+            
+        } catch let error as NSError {
+            print("There was an error processing your request: \(error)")
+        }
+        
+    }
 
     
-    func deleteAllData(entity: String)
-    {
+    func deleteAllData(entity: String) {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
         let managedContext = appDelegate.persistentContainer.viewContext
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
@@ -75,31 +89,23 @@ class CategoryViewController: UIViewController {
             print("Detele all data in \(entity) error : \(error) \(error.userInfo)")
         }
     }
-
-    func clearCoreData(){
-        let delegate = UIApplication.shared.delegate as! AppDelegate
-        let context = delegate.persistentContainer.viewContext
-        
-        for i in 0...delegate.persistentContainer.managedObjectModel.entities.count-1{
-            let entity = delegate.persistentContainer.managedObjectModel.entities[i]
-            
-            do {
-                let query = NSFetchRequest<NSFetchRequestResult>(entityName: entity.name!)
-                let deleteRequest = NSBatchDeleteRequest(fetchRequest: query)
-                try context.execute(deleteRequest)
-                try context.save()
-            }catch let error as NSError{
-                print("Error: \(error.localizedDescription)")
-            }
-        }
-    }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         deleteAllData(entity: "Category")
-        createCategoryData()
-        getCategoryData()
+        createCategoryData(name: "ICT", summary: "Computers and stuff you know")
+        createCategoryData(name: "Humanities and Arts", summary: "punch of hippies")
+        createCategoryData(name: "Social sectors", summary: "Studies and work related to society")
+        createCategoryData(name: "Education Sciences", summary: "")
+        createCategoryData(name: "Trade, Adminstration and Law", summary: "")
+        createCategoryData(name: "Natural Sciences", summary: "")
+        createCategoryData(name: "Technical Fields", summary: "")
+        createCategoryData(name: "Agriculture and Forestry", summary: "")
+        createCategoryData(name: "Health and Wellbeing", summary: "")
+        createCategoryData(name: "Service Industry", summary: "")
+        createCategoryData(name: "General Education", summary: "")
+        createCategoryData(name: "Misc & Unkown", summary: "")
+        getCategoryData(name: "Social sectors")
         // Do any additional setup after loading the view.
         
         //Reminder check for duplicate objects
