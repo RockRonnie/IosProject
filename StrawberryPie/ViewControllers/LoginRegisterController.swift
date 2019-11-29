c0a64c8b59c44ab9d5c83efef7bde05ae6b38c4b
magmake <hepokat@hotmail.com>
2019-11-29 20:47:06 +0200

    signupNextphasev1

--- StrawberryPie/ViewControllers/LoginRegisterController.swift
+++ StrawberryPie/ViewControllers/LoginRegisterController.swift
@@ -16,25 +16,23 @@ import RealmSwift
   let changeFormButton = UIButton(type: .roundedRect)
   let loginButton = UIButton(type: .roundedRect)
   let signUpButton = UIButton(type: .roundedRect)
+  let cancelButton = UIButton(type: .roundedRect)
+  let infoAddedButton = UIButton(type: .roundedRect)
   let errorLabel = UILabel()
+  let userEmailField = UITextField()
   let usernameField = UITextField()
   let firstnameField = UITextField()
   let lastnameField = UITextField()
   let userinfoField = UITextField()
+  let userXtraInfoField = UITextField()
   let userpasswordField = UITextField()
   let userpwagainField = UITextField()
   
   var realm: Realm!
-  var notificationToken: NotificationToken?
   var user: SyncUser?
   let main = UIStoryboard(name: "Main", bundle: nil)
   let thisUser = User()
-  
-  // Tänne pusketaan data --> Vaihda johonkin fiksumpaan
-  private var tableSource = [] as [ChatMessage]
-  
-  //Log user
-  
+
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -50,12 +48,17 @@ import RealmSwift
     userpasswordField.isHidden = false
     userinfoField.isHidden = true
     userpwagainField.isHidden = true
+    userEmailField.isHidden = true
+    userXtraInfoField.isHidden = true
     firstnameField.isHidden = true
     lastnameField.isHidden = true
     loginButton.isHidden = true
     signUpButton.isHidden = true
+    cancelButton.isHidden = true
     changeFormButton.isHidden = false
+    infoAddedButton.isHidden = true
     
+    // Initial swap of forms to get Loginform to show
     self.signUpFormEnabled = true
     self.switchForm()
     
@@ -80,36 +83,56 @@ import RealmSwift
     usernameField.placeholder = "Username"
     usernameField.borderStyle = .roundedRect
     usernameField.autocapitalizationType = .none
-    container.addArrangedSubview(usernameField)
     firstnameField.placeholder = "First name"
     firstnameField.borderStyle = .roundedRect
-    container.addArrangedSubview(firstnameField)
     lastnameField.placeholder = "Last name"
     lastnameField.borderStyle = .roundedRect
-    container.addArrangedSubview(lastnameField)
+    userEmailField.placeholder = "Email"
+    userEmailField.borderStyle = .roundedRect
     userinfoField.placeholder = "Info"
     userinfoField.borderStyle = .roundedRect
-    container.addArrangedSubview(userinfoField)
-    
+    userXtraInfoField.placeholder = "More Info"
+    userXtraInfoField.borderStyle = .roundedRect
     userpasswordField.placeholder = "Password"
     userpasswordField.borderStyle = .roundedRect
     userpasswordField.autocapitalizationType = .none
-    container.addArrangedSubview(userpasswordField)
     userpasswordField.passwordRules = UITextInputPasswordRules(descriptor: "required: upper; required: lower; required: digit; max-consecutive: 2; minlenght: 8;")
     userpwagainField.placeholder = "Password again"
     userpwagainField.borderStyle = .roundedRect
     userpwagainField.autocapitalizationType = .none
+    // Add every text field to container
+    container.addArrangedSubview(usernameField)
+    container.addArrangedSubview(firstnameField)
+    container.addArrangedSubview(lastnameField)
+    container.addArrangedSubview(userEmailField)
+    container.addArrangedSubview(userpasswordField)
     container.addArrangedSubview(userpwagainField)
+    container.addArrangedSubview(userinfoField)
+    container.addArrangedSubview(userXtraInfoField)
+    
     // Buttons
+    // LOGIN BUTTON
     loginButton.setTitle("Login", for: .normal)
     loginButton.addTarget(self, action: #selector(signIn), for: .touchUpInside)
-    container.addArrangedSubview(loginButton)
+    // SIGNUP BUTTON
     signUpButton.setTitle("Sign up", for: .normal)
     signUpButton.addTarget(self, action: #selector(createUser), for: .touchUpInside)
-    container.addArrangedSubview(signUpButton)
+    // DONE BUTTON TO CONTINUE FOR SIGNUP PT. 2
+    infoAddedButton.setTitle("Done", for: .normal)
+    infoAddedButton.addTarget(self, action: #selector(advancedSignup), for: .touchUpInside)
     changeFormButton.addTarget(self, action: #selector(switchForm),
                                for: .touchUpInside)
+    // IF YOU DONT WANT TO SET USER CUSTOMIZATION NOW
+    cancelButton.setTitle("Cancel, you can modify later", for: .normal)
+    cancelButton.addTarget(self, action: #selector(cancelRegister), for: .touchUpInside)
+    
+    // Add buttons, cancelButton for cancelling extra signup details
+    container.addArrangedSubview(loginButton)
+    container.addArrangedSubview(signUpButton)
     container.addArrangedSubview(changeFormButton)
+    container.addArrangedSubview(cancelButton)
+    container.addArrangedSubview(infoAddedButton)
+    
     // Container's location on the screen
     let guide = view.safeAreaLayoutGuide
     NSLayoutConstraint.activate([
@@ -119,13 +142,13 @@ import RealmSwift
       ])
     
     
+    
   }
   // NOT IN USE, might be used later
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     
   }
-  
-  // LOGIN FUNCTION, ALL THE LOGIC NOT IMPLEMENTED, WILL BE SUBJECT TO CHANGE
+  // Login function, check for username and password
   func logIn(_ username: String,_ password: String,_ register: Bool) {
     if usernameField.text == "" {
       self.present(customAlert(title: "uname", reason: "Missing username"), animated: true, completion: nil)
@@ -133,7 +156,7 @@ import RealmSwift
       self.present(customAlert(title: "upw", reason: "Missing password"),
                    animated: true, completion: nil)
     } else {
-      // USER FROM REALM IS AVAILABLE BEFORE IF-statement so doesnt work, FIX PLS
+      // ALL IS GOOD => LOGIN
       loginRealm(username, password, register)
     }
     
@@ -186,7 +209,7 @@ import RealmSwift
   
   
   // SIGNUP FUNCTION EARLY VERSION, SUBJECT TO CHANGE
-  func signUp(_ username: String, _ password: String, _ register: Bool) -> User {
+  func signUp(_ username: String, _ password: String, _ register: Bool) {
     let loggedIn: UITabBarController? = main.instantiateViewController(withIdentifier: "LoggedInTabBar") as? UITabBarController
     if usernameField.text == "" {
       self.present(customAlert(title: "uname", reason: "Missing username"), animated: true, completion: nil)
@@ -227,6 +250,10 @@ import RealmSwift
           try! self.realm.write {
               self.realm.add(self.thisUser)
           }
+          let alert = UIAlertController(title: "Customize?", message: "Do you want to customize your profile now?", preferredStyle: .alert)
+          alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (action: UIAlertAction!) in self.advancedSignup(user: self.thisUser) }))
+          alert.addAction(UIAlertAction(title: "Do it later", style: .cancel, handler: nil))
+          self.present(alert, animated: true)
           print("Realm connection has been setup")
           print("Changing navigators")
           self.present(loggedIn!, animated:true, completion: nil)
@@ -243,14 +270,56 @@ import RealmSwift
       
      
     }
-    
-    return thisUser
   }
+  
   func signIn() {
     logIn(username ?? "", password ?? "", false)
   }
   func createUser() {
+    
     signUp(username ?? "", password ?? "", true)
+  }
+  func cancelRegister() {
+    let loggedIn: UITabBarController? = main.instantiateViewController(withIdentifier: "LoggedInTabBar") as? UITabBarController
+    self.present(loggedIn!, animated: true, completion: nil)
+  }
+  func advancedSignup(user: User) {
+    RealmDB.sharedInstance.user = self.user
+    // Leivotaan realmia varten asetukset. realmURL: Constants.REALM_URL --> Katso Constants.swift
+    let config = self.user?.configuration(realmURL: Constants.REALM_URL, fullSynchronization: true)
+    if let cfg = config {
+      self.realm = try! Realm(configuration: cfg)
+    } else {
+      print("Error")
+      return
+    }
+    self.changeFormButton.isHidden = true
+    self.signUpButton.isHidden = true
+    self.cancelButton.isHidden = false
+    self.userXtraInfoField.isHidden = false
+    self.userinfoField.isHidden = false
+    signUpFormEnabled = true
+    realm = RealmDB.sharedInstance.realm
+    let users = realm.objects(User.self)
+    
+    for user in users {
+      print(user)
+      let thisUserUpdated = User()
+      if user.userID == RealmDB.sharedInstance.user?.identity {
+        thisUserUpdated.firstName = firstnameField.text ?? ""
+        thisUserUpdated.lastName = lastnameField.text ?? ""
+        thisUserUpdated.info = userinfoField.text ?? ""
+        // EXPERT STATUS TO BE IMPLEMENTED LATER
+        thisUserUpdated.userExpert = false
+        thisUserUpdated.userEmail = userEmailField.text ?? ""
+        thisUserUpdated.info = userinfoField.text ?? ""
+        thisUserUpdated.extraInfo = userXtraInfoField.text ?? ""
+        }
+      try! self.realm.write {
+        self.realm.add(thisUserUpdated, update: Realm.UpdatePolicy.modified)
+    }
+    
+    }
   }
   // Change between login / register
   func switchForm() {
@@ -259,12 +328,12 @@ import RealmSwift
       // LOGIN Form chosen -> Show login fields
       signUpFormEnabled = false
       self.messageLabel.text = "Please enter your login information"
-      self.usernameField.isHidden = false
       self.firstnameField.isHidden = true
       self.lastnameField.isHidden = true
-      self.userpasswordField.isHidden = false
       self.userpwagainField.isHidden = true
       self.userinfoField.isHidden = true
+      self.userXtraInfoField.isHidden = true
+      self.userEmailField.isHidden = true
       self.loginButton.isHidden = false
       self.signUpButton.isHidden = true
       changeFormButton.setTitle("Sign up instead", for: .normal)
@@ -273,12 +342,13 @@ import RealmSwift
       signUpFormEnabled = true
       self.messageLabel.text = "Please fill out the registration form"
       // SIGNUP Form chosen -> Show signup fields
-      self.usernameField.isHidden = false
-      self.userpasswordField.isHidden = false
+
       self.userpwagainField.isHidden = false
       self.firstnameField.isHidden = false
       self.lastnameField.isHidden = false
-      self.userinfoField.isHidden = false
+      self.userXtraInfoField.isHidden = true
+      self.userEmailField.isHidden = false
+      self.userinfoField.isHidden = true
       self.loginButton.isHidden = true
       self.signUpButton.isHidden = false
       changeFormButton.setTitle("Already have an account? Login instead", for: .normal)
