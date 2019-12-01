8cc4e9c094d15bd011d59ab739ba32ef524394f5
Arttu Jokinen <arttujo@gmail.com>
2019-12-01 16:05:39 +0200

    Added info button to category content that shows alert with category summary

--- StrawberryPie/ViewControllers/CategoryContentController.swift
+++ StrawberryPie/ViewControllers/CategoryContentController.swift
@@ -15,9 +15,19 @@ class CategoryContentController: UIViewController {
     var topText = String()
     var categoryObject = [NSManagedObject]()
     
+    let button = UIButton()
+    
+    @objc func press() {
+        let summary = categoryObject[0].value(forKey: "categorySummary") as! String
+        let alert = UIAlertController(title: "Summary", message: summary, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = categoryObject[0].value(forKey: "categoryName") as? String
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "Info", style: .plain, target: self, action: #selector(self.press))
         print(topText)
         
         // Do any additional setup after loading the view.
