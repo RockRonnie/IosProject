60c7ebde8be538e23a8866a953ecbea8b7f6391a
Ilias Doukas <ilias.doukas@gmail.com>
2019-12-01 13:20:20 +0200

    hostcard tableview -> uicollectionview + dummypohja uicv

--- StrawberryPie/ViewControllers/QAController.swift
+++ StrawberryPie/ViewControllers/QAController.swift
@@ -32,9 +32,18 @@ class QAController: UIViewController {
         qaTable.dataSource = self
         qaTable.delegate = self
         
+        hostCardCV.dataSource = self
+        hostCardCV.delegate = self
+        
         //viestin kirjoitus piiloon aluksi
         sendButton.isHidden = true
         messageField.isHidden = true
+        
+        let layout = hostCardCV.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+        layout.minimumInteritemSpacing = 5
+        layout.itemSize = CGSize(width: (hostCardCV.frame.size.width - 20)/2, height: hostCardCV.frame.size.height)
+    
     }
     
     func setupNotification() {
@@ -97,7 +106,8 @@ class QAController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     
-    @IBOutlet weak var hostcardTable: UITableView!
+    @IBOutlet weak var hostCardCV: UICollectionView!
+    
     
     @IBAction func chatButton(_ sender: UIButton) {
         // Vaihdetaan cellin pohjaa ja reloadData()
@@ -141,8 +151,75 @@ class QAController: UIViewController {
     
 }
 
-extension QAController:  UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+extension QAController:  UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "hostcardcell", for: indexPath)
+        
+        if indexPath.row == 0  {
+            let title = UILabel(frame: CGRect(x: 0, y: 0, width: cell.bounds.size.width, height: 40))
+            title.textColor = UIColor.black
+            title.text = "Kuva"
+            title.textAlignment = .left
+            cell.contentView.addSubview(title)
+        }
+        else {
+            let titleFont = UIFont.systemFont(ofSize: 15, weight: UIFont.Weight(500))
+            let title = UILabel(frame: CGRect(x: 0, y: 25, width: cell.bounds.size.width, height: 20))
+            title.textColor = UIColor.black
+            title.text = "Nurse"
+            title.font = titleFont
+            title.textAlignment = .center
+            cell.contentView.addSubview(title)
+            
+            let name = UILabel(frame: CGRect(x: 0, y: 50, width: cell.bounds.size.width, height: 20))
+            name.textColor = UIColor.black
+            name.text = "Gertrud Schmitz"
+            name.textAlignment = .center
+            cell.contentView.addSubview(name)
+            
+            let company = UILabel(frame: CGRect(x: 0, y: 75, width: cell.bounds.size.width, height: 20))
+            company.textColor = UIColor.black
+            company.text = "Oy Hospital Ab"
+            company.textAlignment = .center
+            cell.contentView.addSubview(company)
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item + 1)
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//        var width:CGFloat?
+//        var height:CGFloat?
+//
+//
+//        if indexPath.row == 0 {
+//            print ("indexpath oli 0")
+//            width = (view.frame.size.width / 5)
+//            height = view.frame.size.height / 2
+//        }
+//        else {
+//            print ("myös elsessä käytiin")
+//            width = ((view.frame.width / 5) * 3)
+//            height = view.frame.height / 2
+//        }
+//
+//        return CGSize(width: width ?? 10.0, height: height ?? 10.0)
+//    }
+
     // Päivitetään tableviewiin tavarat sisälle
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         var numberOfRows: Int
