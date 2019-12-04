926d9401f75cd988a02df3df0297c99e05df9bc4
iosdev <ron@johnson.dom>
2019-12-04 11:51:34 +0200

    test fixes

--- StrawberryPieTests/HostControllerTest.swift
+++ StrawberryPieTests/HostControllerTest.swift
@@ -10,79 +10,55 @@ import XCTest
 @testable import StrawberryPie
 
 class HostControllerTest: XCTestCase {
-    let home = HomeController()
     let host = HostController()
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     func testFields(){
         //edu
-        /*
-        host.eduTextField.text = "edu"
-        XCTAssert(host.eduTextField.text == "edu", "edu textfield is not correct")
-        host.selectedEducation = host.eduTextField.text
-        XCTAssert(host.selectedEducation == host.eduTextField.text, "selected education is not derp like edu,text")
-        host.eduTextField.text = nil
-        XCTAssert(host.eduTextField == nil, "eduText is not nil")
-        host.selectedEducation = host.eduTextField.text
-        XCTAssert(host.selectedEducation == host.eduTextField.text, "selected education is not nill like edu.text")
+        
+        host.selectedEducation = "derp"
+        XCTAssert(host.selectedEducation == "derp", "selected education is not derp")
+        host.selectedEducation = nil
+        XCTAssert(host.selectedEducation == nil, "selected education is not nill")
         
         //prof
-        host.profTextField.text = "prof"
-        XCTAssert(host.profTextField.text == "pro", "prof textfield is not correct")
-        host.selectedEducation = host.eduTextField.text
-        XCTAssert(host.selectedProfession == host.profTextField.text, "selected profession is not pro like prof.text")
-        host.profTextField.text = nil
-        XCTAssert(host.profTextField == nil, "profText is not nil")
-        host.selectedProfession = host.eduTextField.text
-        XCTAssert(host.selectedProfession == host.eduTextField.text, "selected profession is not nill like prof.text")
+        host.selectedEducation = "prof"
+        XCTAssert(host.selectedProfession == "prof", "selected profession is not prof")
+        host.selectedProfession = nil
+        XCTAssert(host.selectedProfession == nil, "selected profession is not nill")
         
         //title
-        host.titleTextField.text = "title"
-        XCTAssert(host.profTextField.text == "title", "title textfield is not correct")
-        host.sessionTitle = host.titleTextField.text
-        XCTAssert(host.sessionTitle == host.titleTextField.text, "selected title is not title like title.text")
-        host.titleTextField.text = nil
-        XCTAssert(host.titleTextField == nil, "titleText is not nil")
-        host.sessionTitle = host.titleTextField.text
-        XCTAssert(host.sessionTitle == host.titleTextField.text, "selected title is not nill like title.text")
+       
+        host.sessionTitle = "title"
+        XCTAssert(host.sessionTitle == "title", "selected title is not title")
+      
+        host.sessionTitle = nil
+        XCTAssert(host.sessionTitle == nil, "selected title is not nill")
         
         //desc
-        host.descTextView.text = "desc"
-        XCTAssert(host.descTextView.text == "desc", "desc textview is not correct")
-        host.sessionDesc = host.descTextView.text
-        XCTAssert(host.sessionDesc == host.descTextView.text, "selected desc is not desc like desc.text")
-        host.descTextView.text = nil
-        XCTAssert(host.descTextView == nil, "descText is not nil")
-        host.sessionDesc = host.descTextView.text
-        XCTAssert(host.sessionDesc == host.descTextView.text, "selected desc is not nill like the desc.text")
+        
+        host.sessionDesc = "desc"
+        XCTAssert(host.sessionDesc == "desc", "selected desc is not desc")
+        
+        host.sessionDesc = nil
+        XCTAssert(host.sessionDesc == nil, "selected desc is not nill")
         
         //intro
-        host.introTextView.text = "intro"
-        XCTAssert(host.introTextView.text == "intro", "intro textview is not correct")
-        host.sessionIntro = host.introTextView.text
-        XCTAssert(host.sessionIntro == host.introTextView.text, "selected intro is not intro like intro.text")
-        host.introTextView.text = nil
-        XCTAssert(host.introTextView == nil, "introText is not nil")
-        host.sessionIntro = host.introTextView.text
-        XCTAssert(host.sessionIntro == host.introTextView.text, "selected intro is not nill like the intro.text")
-        */
+   
+        host.sessionIntro = "intro"
+        XCTAssert(host.sessionIntro == "intro", "selected intro is not intro")
+       
+        host.sessionIntro = nil
+        XCTAssert(host.sessionIntro == nil, "selected intro is not nill")
+ 
     }
     
     func testFunctions(){
         host.setCategory(category: "Halp")
         XCTAssert(host.selectedCategory == "Halp", "Host category not set to Halp")
         let categories = host.allCategories
-        /*
-        host.setupDatabase()
-        XCTAssert(host.realm != nil, "realm doesnt exist")
-        XCTAssert(host.allCategories != categories, "coredata allcategories doesn't exist")
-         */
-       // host.setupText()
-        /*
-        XCTAssert(host.titleTextField.placeholder == "Session title", "Session title placeholder is wrong")
-        XCTAssert(host.profTextField.placeholder == "Profession", "Session profession placeholder is wrong")
-        XCTAssert(host.eduTextField.placeholder == "Education", "Session education placeholder is wrong")*/
+        XCTAssert(categories == [], "All categories is not an empty array")
     }
     func realmTests(){
         let intro = host.createIntro()
