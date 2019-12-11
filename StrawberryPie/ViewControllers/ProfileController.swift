d34d5e7288313a6444596ec93e4aa229ed266fc8
magmake <hepokat@hotmail.com>
2019-12-11 23:13:49 +0200

    profilecommenting

--- StrawberryPie/ViewControllers/ProfileController.swift
+++ StrawberryPie/ViewControllers/ProfileController.swift
@@ -323,6 +323,6 @@ extension ProfileController: UITableViewDelegate, UITableViewDataSource {
   // Set height
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     return 35
-    }
-  } 
+  }
 }
+
